384a548978fecc892828b7cd1d04a8b919950812
LooSheng <loosheng@outlook.com>
2023-04-07 22:45:10 +0800

    feat: INTERFACE

--- script/theme.ts
+++ script/theme.ts
@@ -463,6 +463,13 @@ export default function getEditorSchemeTheme({ style, name, soft = false, black 
   attributesElement.ele('option', { name: 'JS.GLOBAL_FUNCTION', baseAttributes: 'DEFAULT_FUNCTION_DECLARATION' })
   attributesElement.ele('option', { name: 'JS.GLOBAL_VARIABLE', baseAttributes: 'DEFAULT_GLOBAL_VARIABLE' })
   attributesElement.ele('option', { name: 'JS.LOCAL_VARIABLE', baseAttributes: 'DEFAULT_LOCAL_VARIABLE' })
+  //   <option name="DEFAULT_INTERFACE_NAME">
+  //   <value>
+  //     <option name="FOREGROUND" value="6440ba" />
+  //   </value>
+  // </option>
+  const DEFAULT_INTERFACE_NAME = attributesElement.ele('option', { name: 'DEFAULT_INTERFACE_NAME' }).ele('value')
+  DEFAULT_INTERFACE_NAME.ele('option', { name: 'FOREGROUND', value: vitesse('interface') })
   return {
     editorTheme: theme.end({ pretty: true }),
     UITheme: {
